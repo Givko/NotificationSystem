3742c9c20e384c3b81c71b0135d878e4714d40a4
Zhivko Milev <z.milev@draftkings.com>
2025-03-16 01:04:46 +0200

    Add viper, gin and kafka producer

--- notification-service/cmd/main.go
+++ notification-service/cmd/main.go
@@ -1,27 +1,9 @@
 package main
 
 import (
-	"os"
-
-	"github.com/rs/zerolog"
+	"github.com/Givko/NotificationSystem/notification-service/internal/server"
 )
 
 func main() {
-	zerolog.TimeFieldFormat = zerolog.TimeFormatUnixMs
-
-	var zl zerolog.Logger
-	if os.Getenv("ENV") == "dev" {
-		zl = zerolog.New(zerolog.ConsoleWriter{
-			Out:        os.Stdout,
-			TimeFormat: "15:04:05",
-		}).With().Timestamp().Logger()
-	} else {
-		//Log into file in order for the logs to be persisted\
-		file, err := os.OpenFile("logs.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-		if err != nil {
-			panic(err)
-		}
-		zl = zerolog.New(file).With().Timestamp().Logger()
-	}
-
+	server.InitServer()
 }
